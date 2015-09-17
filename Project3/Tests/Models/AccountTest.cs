7da2f6df9d1f1a2d5586dd6b537bc07871620897
gblosser <gblosser@tsdweb.com>
2015-09-17 13:15:06 -0400

    Added tests for Account

---
+++ Project3/Tests/Models/AccountTest.cs
@@ -0,0 +1,34 @@
+﻿using Project3.Models;
+using Xunit;
+
+namespace Project3.Tests.Models
+{
+	public class AccountTest
+	{
+		private readonly Account _account;
+		private readonly EventLog _eventLog;
+
+		public AccountTest()
+		{
+			_eventLog = new EventLog();
+			_account = new Account(10, _eventLog);	// instantiate account for test with interest rate of 10%
+		}
+
+		[Fact]
+		public void DepositTest()
+		{
+			_account.Deposit(3);
+			Assert.Equal(3, _account.Balance);
+		}
+
+		[Fact]
+		public void InterestTest()
+		{
+			_account.Deposit(1);
+			_account.Deposit(1);
+			_account.Deposit(1);
+
+			Assert.Equal(3.3m, _account.Balance);
+		}
+	}
+}

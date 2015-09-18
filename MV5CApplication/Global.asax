db990d6617f24535ea5c99fd830a81f44e777bd9
gblosser <gblosser@tsdweb.com>
2015-09-18 16:59:32 -0400

    Added MVC5Application project

---
+++ MV5CApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MV5CApplication.MvcApplication" Language="C#" %>
---
+++ MV5CApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MV5CApplication.Startup))]
+namespace MV5CApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
